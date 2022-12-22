9fb4ab767616b6691b3b6e70d9867ac24ee4b1ac
Brian Hicks <brian@brianthicks.com>
2022-12-22 05:50:33 -0600

    discover files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use color_eyre::Result;
+use eyre::WrapErr;
 use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
@@ -10,8 +11,36 @@ struct Opts {
 
 impl Opts {
     fn run(&self) -> Result<()> {
-        println!("{self:#?}");
+        let files = self.discover_files();
+        println!("{files:#?}");
+
         Ok(())
+    }
+
+    fn discover_files(&self) -> Result<Vec<PathBuf>> {
+        let mut types_builder = ignore::types::TypesBuilder::new();
+        types_builder.add_defaults();
+        types_builder.select("markdown");
+
+        let mut out = Vec::new();
+        for entry_res in ignore::WalkBuilder::new(&self.root)
+            .types(
+                types_builder
+                    .build()
+                    .wrap_err("could not build matching types")?,
+            )
+            .build()
+        {
+            match entry_res {
+                Ok(entry) => match entry.file_type() {
+                    Some(ft) if ft.is_file() => out.push(entry.into_path()),
+                    _ => continue,
+                },
+                Err(problem) => return Err(problem).wrap_err("could not read files"),
+            }
+        }
+
+        Ok(out)
     }
 }
 
